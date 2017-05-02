99676e1b15cdc5474edc91fbf8d7c77e0ed675ef
gems download installer <mhamidshaikh97@gmail.com>
2017-05-02 10:44:02 +0500

    articles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,24 @@ def create
 	
 end
 
+
+
+def show  
+  @post = Post.find(params[:id])
+end
+
+def edit  
+	 @post = Post.find(params[:id])
+end
+
+def update
+ @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to(post_path(@post))  
+end 
+
+
+
 private
 
 def post_params  
